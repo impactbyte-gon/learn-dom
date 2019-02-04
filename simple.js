a17ea1fbed8d70c0a409094952599a288cfcf434
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-04 14:40:00 +0700

    Improve examples

--- simple.js
+++ simple.js
@@ -1,61 +1,20 @@
-// ---------------------------------------------------------------------------
-// ELEMENTS
 const taskListDOM = document.getElementById('task-list')
 
-// ---------------------------------------------------------------------------
-// PROGRAM
 const App = {
-  // ---------------------------------------------------------------------------
-  // DATA
   nextId: 6,
-  data: [
-    {
-      id: 1,
-      text: 'Learning JavaScript',
-      completed: true
-    },
-    {
-      id: 2,
-      text: 'Running a Hackathon',
-      completed: false
-    },
-    {
-      id: 3,
-      text: 'Flying an Airplane',
-      completed: false
-    },
-    {
-      id: 4,
-      text: 'Building a house',
-      completed: false
-    },
-    {
-      id: 5,
-      text: 'Drawing a mountain',
-      completed: false
-    }
-  ],
-
-  // ---------------------------------------------------------------------------
-  // DISPLAY DATA
+  data: [],
   display: (data = App.data) => {
     taskListDOM.innerHTML = ''
-
     data.forEach(item => {
       const li = document.createElement('li')
-
       li.innerHTML = `<span ${
         item.completed ? 'class="completed"' : ''
       } onclick="App.toggleCompleted(${item.id})">${item.text}</span>
       <button onclick="App.edit(${item.id})">✎ EDIT</button>
       <button onclick="App.remove(${item.id})">✖ REMOVE</button>`
-
       taskListDOM.appendChild(li)
     })
   },
-
-  // ---------------------------------------------------------------------------
-  // SUBMIT NEW DATA
   add: () => {
     event.preventDefault()
     const newTask = {
@@ -63,7 +22,6 @@ const App = {
       text: document.getElementById('add-text').value,
       completed: false
     }
-
     if (newTask !== '') {
       App.data.push(newTask)
       App.display()
@@ -71,23 +29,15 @@ const App = {
       App.nextId++
     }
   },
-
-  // ---------------------------------------------------------------------------
-  // REMOVE TASK BY ID
   remove: id => {
     const modifiedTasks = App.data.filter(item => {
       return item.id !== id
     })
-
     App.data = modifiedTasks
     App.display()
   },
-
-  // ---------------------------------------------------------------------------
-  // EDIT TASK TEXT
   edit: id => {
     const textInput = prompt('Edit task to...')
-
     if (textInput !== null) {
       const modifiedTasks = App.data.map(item => {
         if (item.id === id) {
@@ -95,39 +45,25 @@ const App = {
         }
         return item
       })
-
       App.data = modifiedTasks
       App.display()
     }
   },
-
-  // ---------------------------------------------------------------------------
-  // SEARCH TASK AFTER SUBMIT
   search: () => {
     event.preventDefault()
     const keyword = document.getElementById('search-text').value
-
     const foundTasks = App.data.filter(item => {
       return item.text.toLowerCase().includes(keyword.toLowerCase())
     })
-
     App.display(foundTasks)
   },
-
-  // ---------------------------------------------------------------------------
-  // SEARCH TASK AUTOMATICALLY
   searchAuto: () => {
     const keyword = document.getElementById('search-text').value
-
     const foundTasks = App.data.filter(item => {
       return item.text.toLowerCase().includes(keyword.toLowerCase())
     })
-
     App.display(foundTasks)
   },
-
-  // ---------------------------------------------------------------------------
-  // TOGGLE TASK COMPLETED
   toggleCompleted: id => {
     const modifiedTasks = App.data.map(item => {
       if (item.id === id) {
@@ -135,12 +71,9 @@ const App = {
       }
       return item
     })
-
     App.data = modifiedTasks
     App.display()
   }
 }
 
-// ---------------------------------------------------------------------------
-// RUN
 App.display()
